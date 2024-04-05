4f41e78ab94ed5674e041bc53e952743810b5be5
Fyphen <fyphensub@gmail.com>
2024-04-05 15:18:47 +0900

    come on Electron forge

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+const path = require('path');
+
 const { app, BrowserWindow } = require('electron');
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, '/assets/js/preload.js'),
+        },
     });
 
     win.loadFile('./assets/html/index.html');
@@ -11,4 +16,8 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
     createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
 });
